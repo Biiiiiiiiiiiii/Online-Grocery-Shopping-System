bd8f0a4e5498730fedffa21d6ff76667c82a4308
meitingyau <65650308+meitingyau@users.noreply.github.com>
2021-06-01 18:33:14 +0800

    Update productscript.js

--- assignment_part2/productscript.js
+++ assignment_part2/productscript.js
@@ -1,58 +1,59 @@
 
 
-$(".filter").on("keyup",function(){
+$(".filter").on("keyup", function () {
     var input = $(this).val().toUpperCase();
-    $(".card").each(function(){
-        if($(this).data("string").toUpperCase().indexOf(input)<0){
-            $(this).css({display:"none"});
-            
-        } else{
-            $(this).css({display:""});
-            
+    $(".card").each(function () {
+        if ($(this).data("string").toUpperCase().indexOf(input) < 0) {
+            $(this).css({ display: "none" });
+
+        } else {
+            $(this).css({ display: "" });
+
         }
     })
 });
 
 
-    function plus(e){
-        var quantity = e.parentElement.querySelector("#quantity");
-        var count = parseInt(quantity.value);
-        count++;
+function plus(e) {
+    var quantity = e.parentElement.querySelector("#quantity");
+    var count = parseInt(quantity.value);
+    count++;
+    quantity.value = count;
+};
+function minus(e) {
+    var quantity = e.parentElement.querySelector("#quantity");
+    var count = parseInt(quantity.value);
+    if (count > 1) {
+        count--;
         quantity.value = count;
-    };
-    function minus(e){
-        var quantity = e.parentElement.querySelector("#quantity");
-        var count = parseInt(quantity.value);
-            if(count>1){
-                count--;
-                quantity.value = count;
-            }   
-    };
+    }
+};
 
 
-function openModal(e){
+function openModal(e) {
     document.getElementById("listModal").style.display = "block";
     var quantity = e.parentElement.querySelector("#quantity");
     var id = e.parentElement.querySelector("#PID");
-    document.getElementById('productQuantity').value=quantity.value;
-    document.getElementById('productID').value=id.value;
+    document.getElementById('productQuantity').value = quantity.value;
+    document.getElementById('productID').value = id.value;
 }
-function closeModal(){
+function closeModal() {
     document.getElementById("listModal").style.display = "none";
 }
 
-function popOut(_id){
+function popOut() {
     if (confirm("Are you sure to delete this product?")) {
-        var product_id = "id"+_id;
-        document.getElementById(product_id).style.display='none';
+        $(".con").val("Yes")
+    }
+    else {
+        $(".con").val("")
     }
 }
-function popmessage(){
+function popmessage() {
     alert("Your changes have been saved");
-  }
-
-  function popUp(){
-    alert("Please login to access this feature!");
 }
 
+function popUp() {
+    alert("Please login to access this feature!");
+}
 
