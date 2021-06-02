1cef9094540e9c1091fb1539214fcc6dd3134483
Beatricia Teh <58943667+Biiiiiiiiiiiii@users.noreply.github.com>
2021-06-02 12:55:25 +0800

    Fix h1 and title

--- assignment_part2/dry_admin.php
+++ assignment_part2/dry_admin.php
@@ -11,7 +11,7 @@ include("config.php");
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="style.css">
-    <title>Products</title>
+    <title>Dry food</title>
 </head>
 
 <body>
@@ -64,7 +64,7 @@ include("config.php");
         </div>
     </nav>
     <div class="wrapper">
-        <div class="h1">Products</div>
+        <div class="h1">Dry food</div>
         <section id="sidebar" style="margin-right: 20px;">
             <div class="py-3">
                 <h5 class="font-weight-bold">Categories</h5>
