4d5f4b02aca0f6b148e34d26e0514b089a57371b
Beatricia Teh <58943667+Biiiiiiiiiiiii@users.noreply.github.com>
2021-06-12 18:36:17 +0800

    Update check.php

--- assignment_part2/check.php
+++ assignment_part2/check.php
@@ -1,6 +1,11 @@
 <?php
     session_start();
-    $db = mysqli_connect('localhost','root','','shoppinglah');
+    $host = 'localhost';
+    $dbname = 'shoppinglah';
+    $user = "admin"; 
+    $password = "shoppinglah"; 
+    $db = mysqli_connect($host,$user,$password,$dbname);
+    //$db = mysqli_connect('localhost','root','','shoppinglah');
     if ($db->connect_errno) {
         printf("Connect failed: %s\n", $db->connect_error);
         exit();
@@ -29,15 +34,31 @@
         }else if(empty($password)){
             exit();
         }else{
-            $sql= "SELECT * FROM signup WHERE email='$email' AND password='$password'";
+            $hashedPassword = sha1($password);
+            $sql= "SELECT * FROM users WHERE email='$email' AND password='$hashedPassword'";
             $result = mysqli_query($db,$sql);
-            $rowcount = mysqli_num_rows($result);
-            $getresult = $result-> fetch_row();
-            if($rowcount>=1){
-                if($getresult[3]===$email && $getresult[4]===$password ){
-                  if($getresult[5]==="Admin"){
+            //$rowcount = mysqli_num_rows($result);
+            
+            if($result->num_rows >0){
+                while($res=$result->fetch_assoc()){
+                  $userid = $res['userid'];
+                  $name = $res['name'];
+                  $emaill = $res['email'];
+                  $pw=$res['password'];
+                  $level=$res['level'];
+                }
+                //register session variable
+                
+            
+                if($emaill===$email && $pw===$hashedPassword ){
+                    $_SESSION['logged_in'] = true;
+                    $_SESSION['user_id'] = $userid;
+                    $_SESSION['user_name'] = $name;
+                    $_SESSION['user_email'] = $email;//$getresult = $result-> fetch_row();
+
+                  if($level==="Admin"){
                     echo '<script>alert("Log in as admin successfully")</script>';
-                    echo "<script>window.location='home_admin.php';</script>";
+                    
                     if(!empty($remember)){
                         setcookie("email",$email,time()+3600);
                         setcookie("password",$password,time()+3600);
@@ -45,14 +66,22 @@
                         setcookie("email","");
                         setcookie("password","");
                     }
+                    header("Location: home_admin.php?action=login_success");
                   }else {
                     echo '<script>alert("Log in as user successfully")</script>';
-                    echo "<script>window.location='home_login.php';</script>";
+                    if(!empty($remember)){
+                        setcookie("email",$email,time()+3600);
+                        setcookie("password",$password,time()+3600);
+                    }else{
+                        setcookie("email","");
+                        setcookie("password","");
+                    }
+                    header("Location: home_login.php?action=login_success");
                   }
                 }
             }else{
                 echo '<script>alert("Username or Password is invalid")</script>';
-                echo "<script>window.location='login.php';</script>";
+                header("Location: login.php?action=login_failed");
         }
     }
 }else{
