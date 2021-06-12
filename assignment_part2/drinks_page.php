74691265aedbbf3f7ad6f8b507e97b824bd12fc1
Beatricia Teh <58943667+Biiiiiiiiiiiii@users.noreply.github.com>
2021-06-12 18:38:12 +0800

    Update drinks_page.php

--- assignment_part2/drinks_page.php
+++ assignment_part2/drinks_page.php
@@ -2,6 +2,8 @@
 <?php
 session_start();
 include("config.php");
+if (isset($_SESSION['logged_in']) && $_SESSION['user_id'] && $_SESSION['user_email'] && $_SESSION['logged_in']==true ){
+    $id = $_SESSION['user_id'];
 ?>
 <html>
 
@@ -14,7 +16,14 @@ include("config.php");
     <link rel="stylesheet" href="style.css">
     <title>Drinks</title>
 </head>
+<?php
+ //fetch data from db
+ $getpp = "SELECT * FROM users WHERE userid='$id'";
+ $query = $mysqli->query($getpp);
 
+ while($pp=mysqli_fetch_array($query)){
+
+?>
 <body>
     <header>
 
@@ -32,7 +41,7 @@ include("config.php");
     </header>
 
     <nav class="navbar navbar-expand-md navbar-dark fixed-top " style="background-color: #eacefe; ">
-        <a href="home_login.html"> <img src="logo.jpg " alt="logo " width="150 " height="40 "></a>
+        <a href="home_login.php"> <img src="logo.jpg " alt="logo " width="150 " height="40 "></a>
         <button class="navbar-toggler " type="button " data-toggle="collapse " data-target="#navbarCollapse "
             aria-controls="navbarCollapse " aria-expanded="false " aria-label="Toggle navigation ">
             <span class="navbar-toggler-icon "></span>
@@ -40,7 +49,7 @@ include("config.php");
         <div class="collapse navbar-collapse " id="navbarCollapse ">
             <ul class="navbar-nav mr-auto ">
                 <li class="nav-item ">
-                    <a class="nav-link " style="color: #541191; " href="home_login.html">Home</a>
+                    <a class="nav-link " style="color: #541191; " href="home_login.php">Home</a>
                 </li>
                 <li class="nav-item dropdown ">
                     <div class="btn-group">
@@ -59,20 +68,18 @@ include("config.php");
                     </div>
                 </li>
                 <li class="nav-item ">
-                    <a class="nav-link " style="color: #541191; " href="shoppinglist.html">My Shopping List</a>
+                    <a class="nav-link " style="color: #541191; " href="shoppinglist.php">My Shopping List</a>
                 </li>
                 <li class="nav-item ">
-                    <a class="nav-link " style="color: #541191; " href="history.html">History</a>
+                    <a class="nav-link " style="color: #541191; " href="history.php">History</a>
                 </li>
             </ul>
             <ul class="nav ">
-                <li class="nav-item "><a href="index.html" class="nav-link px-2 " style="color: #541191; ">Logout</a>
-                </li>
-                <li class="nav-item "><a href="userprofile.html" class="nav-link px-2 " style="color: #541191; ">
-                        <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="User" class="rounded-circle"
-                            width="30"></a></li>
-            </ul>
-
+                <li class="nav-item "><a href="logout.php" class="nav-link px-2 " style="color: #541191; ">Logout</a></li>
+                <li class="nav-item "><a href="profile.php" class="nav-link px-2 " style="color: #541191; ">
+                        <img <?php echo 'src="data:image/jpeg;base64,' . base64_encode($pp['pic']) . '"' ?>  alt="User" class="rounded-circle" width="30"></a></li>
+                    </ul>
+            <?php } ?>
         </div>
     </nav>
 
@@ -80,8 +87,8 @@ include("config.php");
     <?php
         $submit=isset($_GET['submit']) ? $_GET['submit'] : NULL;
         if($submit=="Add"){
-            echo "<div class='alert alert-success' role='alert'> Product added to shopping list.  
-            <strong><a href='shoppinglist.html' class='alert-link'>View shopping list</a></strong></div>";
+            echo "<h5 style='text align:centre;color:slateblue'> Product added to shopping list.  
+            <strong><a href='shoppinglist.php' class='alert-link'>View shopping list</a></strong></h5>";
         }
         ?>
         <div class="h1">Drinks</div>
@@ -133,7 +140,7 @@ include("config.php");
                 <div class="container py-3">
                     <div class="row">
                         <?php
-                                $list=mysqli_query($mysqli,"SELECT * FROM shoppinglist");
+                                $list=mysqli_query($mysqli,"SELECT * FROM shoppinglist WHERE Status='Active' AND userid='$id'");
                                 $query=mysqli_query($mysqli,"SELECT * FROM product WHERE ProductCategory='Drinks'");
                                 while($row=mysqli_fetch_array($query)):
                             ?>
@@ -227,7 +234,7 @@ include("config.php");
                 <div class="col-11">
                     <div class="row ">
                         <div class="col-xl-8 col-md-4 col-sm-4 col-12 my-auto mx-auto a">
-                            <a href="home_login.html"> <img src="logo.jpg" alt="logo " width="250 " height="70 "></a>
+                            <a href="home_login.php"> <img src="logo.jpg" alt="logo " width="250 " height="70 "></a>
                             <!-- <h3 class="text-muted mb-md-0 mb-5 bold-text">Shoppinglah</h3> -->
                         </div>
                         <div class="col-xl-2 col-md-4 col-sm-4 col-12">
@@ -273,6 +280,6 @@ include("config.php");
 </body>
 
 </html>
-<?php
+<?php }
 mysqli_close($mysqli);
 ?>
