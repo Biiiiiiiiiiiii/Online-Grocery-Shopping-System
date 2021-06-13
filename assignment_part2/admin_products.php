41eb86e420c5a9c4ad5ccb0d7750c39a33ac9e00
Beatricia Teh <58943667+Biiiiiiiiiiiii@users.noreply.github.com>
2021-06-13 11:36:01 +0800

    Fix profile bug

--- assignment_part2/admin_products.php
+++ assignment_part2/admin_products.php
@@ -67,7 +67,7 @@ if (isset($_SESSION['logged_in']) && $_SESSION['user_id'] && $_SESSION['user_ema
             </ul>
             <ul class="nav ">
                 <li class="nav-item "><a href="logout.php" class="nav-link px-2 " style="color: #541191; ">Logout</a></li>
-                <li class="nav-item "><a href="profile.php" class="nav-link px-2 " style="color: #541191; ">
+                <li class="nav-item "><a href="adminprofile.php" class="nav-link px-2 " style="color: #541191; ">
                         <img <?php echo 'src="data:image/jpeg;base64,' . base64_encode($pp['pic']) . '"' ?>  alt="User" class="rounded-circle" width="30"></a></li>
                     </ul>
         <?php } ?>
